e76dd864a3166e19d9c4228fae86a1c612d7d733
sethc23 <sethc23@users.noreply.github.com>
2016-04-09 22:32:05 -0400

    added commentted content not previously included since recreating sysparse from pyc after connection and data loss issue

--- sysparse.py
+++ sysparse.py
@@ -97,6 +97,10 @@ def arg_by_pgsql(*args, **kwargs):
             print 'ERROR -- dropping to ipy'
             i_trace()
 
+        # for it in ['action','default','choices']:
+        #     if declared_args.has_key(it):
+        #         del declared_args[it]
+
         setattr(func, 'res', sorted([ it['res'] for it in j_res ]))
         return func
 
@@ -122,9 +126,13 @@ def arg_by_shell(*args, **kwargs):
                 run_cmd = True
         if not run_cmd:
             RES = []
+         # os.system(                                     "logger -t 'PY_PARSE_EXEC_1' '%s'" % str(cmd))
         _out, _err = sub_popen(cmd.replace('\n', ''), stdout=sub_PIPE, shell=True).communicate()
         assert _err is None
         RES = _out
+        # for it in ['action','default','choices']:
+        #     if declared_args.has_key(it):
+        #         del declared_args[it]
         setattr(func, 'res', RES)
         return func
 
@@ -168,6 +176,10 @@ class Store_File(argparse.Action):
         super(Store_File, self).__init__(option_strings, dest, **kwargs)
 
     def __call__(self, parser, namespace, values, option_string = None):
+        # print '%r %r %r' % (namespace, values, option_string)
+        # try:
+        # def hook_nobuf(filename, mode):
+        #     return open(filename, mode, 0)
         global input_text
         print 'here too'
         if values:
@@ -207,6 +219,7 @@ def parse_choices_from_exec(cmd, parsed_args = []):
             run_cmd = True
     if not run_cmd:
         return []
+    # os.system(                                     "logger -t 'PY_PARSE_EXEC_1' '%s'" % str(cmd))
     _out, _err = sub_popen(cmd.replace('\n', ''), stdout=sub_PIPE, shell=True).communicate()
     assert _err is None
     return _out
@@ -248,6 +261,10 @@ def parse_choices_from_pgsql(query, parsed_args = []):
 
 
 def parser_completer(prefix, parsed_args, **kwargs):
+    # os.system(                             "logger -t 'PY_PARSE_COMPLETER' '%s'" % prefix )
+    # os.system(                             "logger -t 'PY_PARSE_COMPLETER' '%s'" % parsed_args)
+    # os.system(                             "logger -t 'PY_PARSE_COMPLETER' '%s'" % kwargs )
+    # os.system(                             "logger -t 'PY_PARSE_COMPLETER' '%s'" % kwargs['action'].choices )
     return (it for it in kwargs['action'].choices if it.find(prefix) == 0)
 
 
@@ -258,6 +275,7 @@ def parse_function(class_func, parser, in_args):
     p.description = class_func.__doc__
     p.usage = '\n\t' + ' '.join([p.usage.replace(' [--help]', '').strip('\n'), THIS_ARG])
     pre_existing_args = p.usage.split()[1:]
+    # Special Case (need to include default arg when parsing command line)
     if single_class:
         pre_existing_args = [single_class]
     t = inspect.stack()[:]
@@ -267,6 +285,7 @@ def parse_function(class_func, parser, in_args):
             arg_specs = inspect.getargspec(inspect.getargvalues(it[0]).locals['class_func'])
             break
 
+    # if hasattr(class_func,'argh_args'):
     arg_defaults = None if not arg_specs.defaults else dict(zip(arg_specs.args[-len(arg_specs.defaults):], arg_specs.defaults))
     for i in range(len(class_func.argh_args)):
         arg_name, arg_names, arg_val = (None, None, None)
@@ -276,10 +295,14 @@ def parse_function(class_func, parser, in_args):
             it['choices'] = it['completer']
             if not os.environ.has_key('COMP_LINE'):
                 del it['completer']
+        
+        # Get Arg Info
         arg_names = it['option_strings']
         if not len(arg_names):
             break
         arg_name = arg_names[0]
+
+        # Get Value of Positional Arg (if any)
         if not arg_name[0] == '-':
             if i > len(non_help_in_args) - 1:
                 arg_val = ''
@@ -287,44 +310,75 @@ def parse_function(class_func, parser, in_args):
                 arg_val = non_help_in_args[i]
                 if not arg_val:
                     arg_val_is_null = True
-        elif it.has_key('dest') and it['dest'] == 'input':
-            if globals().has_key('input_text') and input_text:
-                arg_val = input_text
-                idx = in_args.index('---')
-                last_args.extend([in_args.pop(idx), input_text])
-            else:
-                arg_val_is_null = True
-        else:
-            for _arg in arg_names:
-                if non_help_in_args.count(_arg):
-                    idx = non_help_in_args.index(_arg)
-                    try:
-                        arg_val = non_help_in_args[idx + 1]
-                    except:
-                        print it
-                        print _arg
-                        i_trace()
 
-                    if not arg_val:
-                        arg_val_is_null = True
-                    break
+        # Get Value of Optional Arg (if any)
+        else:
+
+            # Special Case for Piped Data
+            elif it.has_key('dest') and it['dest'] == 'input':
+                if globals().has_key('input_text') and input_text:
+                    arg_val = input_text
+                    idx = in_args.index('---')
+                    last_args.extend([in_args.pop(idx), input_text])
+                else:
+                    arg_val_is_null = True
+                    
+            # Normal Case
+            else:
+                for _arg in arg_names:
+                    if non_help_in_args.count(_arg):
+                        idx = non_help_in_args.index(_arg)
+                        try:
+                            arg_val = non_help_in_args[idx + 1]
+                        except:
+                            print it
+                            print _arg
+                            i_trace()
+
+                        if not arg_val:
+                            arg_val_is_null = True
+                        break
 
         del it['option_strings']
+
+        # CASE: arg value in command line
+        #   include arg in usage
+        #   suppress help
+
         if arg_val:
             it['help'] = argparse.SUPPRESS
             p.usage += ' ' + arg_val
+            # CASE: if arg_val is not contained in choices, e.g., partial arg val
             if it.has_key('choices'):
                 if not it['choices'].count(arg_val):
                     tmp_p = argcomplete.argparse.ArgumentParser(add_help=False)
                     tmp_p.add_argument(*arg_names, **it).completer = parser_completer
                     tmp_p = add_arg_help(tmp_p)
                     argcomplete.autocomplete(tmp_p)
+        # CASE: no arg value in command line
+        #   (generally):
+        #       add line with CHOICES if applicable
+        #       add line with DEFAULT
+        #
+        #   usage:
+        #       if positional, make all caps
+        #       if optional, enclose with brackets
+        #
+        #   positional help text:
+        #       use arg_name.upper() as metavar
+        #       
+        #   optional help text:
+        #       if applicable, add line "NOTE: argument can be specified multiple times"
+        # 
         else:
             if not arg_names[0][0] == '-' and not arg_name[0] == '-':
                 p.usage += ' ' + arg_name.upper()
                 it['metavar'] = arg_name.upper()
                 if it.has_key('type'):
                     print 'last_here'
+                # if, while iterating through current command line arg, 
+                #   a positional argument is reached without a value,
+                #   create temp. parser and run autocomplete function for said positional arg
                 tmp_p = argcomplete.argparse.ArgumentParser(add_help=False)
                 tmp_p.add_argument(*arg_names, **it).completer = parser_completer
                 tmp_p = add_arg_help(tmp_p)
@@ -350,6 +404,7 @@ def parse_function(class_func, parser, in_args):
                     it['help'] += '\nDEFAULT: %s' % it['default']
         if it.has_key('default') and not in_args and not arg_name[0] == '-':
             in_args.append(it['default'])
+        # Removing null values from command line
         if arg_val_is_null:
             if in_args.count(arg_name):
                 idx = in_args.index(arg_name)
@@ -360,15 +415,25 @@ def parse_function(class_func, parser, in_args):
             p.add_argument(*arg_names, **it)
 
     p.usage += ' [--help]\n'
+    # If Class/Module only has one function, the function is called by default but need not show in usage
     if single_function:
         p.usage = p.usage.replace(' %s ' % single_function, ' ')
     if not in_args or os.environ.has_key('COMP_LINE'):
         argcomplete.autocomplete(p)
         return p.parse_known_args()
+        # return argh.dispatch(p)
     if in_args.count('--help'):
         p.print_help()
         exit(1)
     else:
+        # print pre_existing_args + in_args + last_args
+        # i_trace()
+
+        # if os.environ.has_key('COMP_LINE'):
+        #     os.system(                         "logger -t 'PY_PARSE_F2' '%s'" % str('>>'+os.environ['COMP_LINE']+'<<'))
+        # else:
+        #     os.system(                         "logger -t 'PY_PARSE_F2' '%s'" % str('>>'+str(in_args)+'<<'))
+        
         if single_class and single_function and in_args == [single_function]:
             return class_func()
         return p.parse_args(pre_existing_args + in_args + last_args)
@@ -378,6 +443,7 @@ def parse_module_or_class(mod_class, parser, in_args, parse_type = 'class'):
     global single_function
     p = parser
     non_help_in_args = [ it for it in in_args if not it == '--help' ]
+    # function is being pulled from command line
     in_args = in_args if in_args == ['--help'] else in_args[1:]
     THIS_ARG = '' if not non_help_in_args else non_help_in_args[0]
     p.description = mod_class.__doc__
@@ -386,31 +452,45 @@ def parse_module_or_class(mod_class, parser, in_args, parse_type = 'class'):
         fxs = [ name for name, fx in inspect.getmembers(mod_class, inspect.ismethod) if hasattr(fx, 'argh_args') ]
     elif parse_type == 'module':
         fxs = [ name for name, fx in inspect.getmembers(mod_class, inspect.isfunction) if hasattr(fx, 'argh_args') ]
+        # fxs                                 =   [name for name,fx in inspect.getmembers(mod_class,inspect.isfunction)]
+    # If single-class, clean usage
     if no_class or single_class:
         p.usage = p.usage.replace(' %s ' % THIS_ARG, ' ')
+    # If Class only has one function, this function is called by default
     if len(fxs) == 1:
         single_function = fxs[0]
         if not in_args == ['--help']:
             in_args.insert(0, single_function)
     else:
         single_function = False
+    # Parsing Class Ends Here
     if not in_args or in_args == ['--help']:
         sp = p.add_subparsers()
         for it in fxs:
             sp.add_parser(it, help=getattr(mod_class, it).__doc__)
-
+        # if os.environ.has_key('COMP_LINE'):
+        #     os.system(                         "logger -t 'PY_PARSE_C1' '%s'" % str('>>'+os.environ['COMP_LINE']+'<<'))
+        # else:
+        #     os.system(                         "logger -t 'PY_PARSE_C1' '%s'" % str('>>'+str(in_args)+'<<'))
         p.usage = '\n\t' + p.format_usage().replace('usage: ', '', 1).replace('[--help]', '').replace(' ... ', '').rstrip(' \n')
         sp.metavar = 'FUNCTION'
         p.usage += ' %s [--help]' % sp.metavar
         argcomplete.autocomplete(p)
         return p.parse_args()
+    # Unrecognized Parse instruction
     if in_args and not fxs.count(in_args[0]):
         argcomplete.autocomplete(p)
         print 'Unrecognized <function>'
         print in_args
         p.print_help()
         exit(1)
+    # Execute Parse instruction
     else:
+        # if os.environ.has_key('COMP_LINE'):
+        #     os.system(                         "logger -t 'PY_PARSE_C3' '%s'" % str('>>'+os.environ['COMP_LINE']+'<<'))
+        # else:
+        #     os.system(                         "logger -t 'PY_PARSE_C3' '%s'" % str('>>'+str(in_args)+'<<'))
+        #     os.system(                         "logger -t 'PY_PARSE_C3' '%s'" % str('>>'+str(p.usage)+'<<'))
         p_help = argparse.SUPPRESS if [ it for it in in_args if not it == '--help' ] else mod_class.__doc__
         p.add_argument('_func', choices=fxs, help=p_help)
         return parse_function(getattr(mod_class, in_args[0]), p, in_args)
@@ -431,23 +511,35 @@ def parse_module(mod_path, regex, excludes):
         elif not regex and inspect.isclass(v) and not v.__module__ and not excludes.count(k):
             avail_classes.update({k: k.lower()})
 
+    # Check input args and use autocomplete for partial args
     if in_args and (not in_args[0] == '--help' or not avail_classes.values().count(in_args[0])):
+        # if os.environ.has_key('COMP_LINE'):
+        #     os.system(                         "logger -t 'PY_PARSE_M1' '%s'" % str('>>'+os.environ['COMP_LINE']+'<<'))
+        # else:
+        #     os.system(                         "logger -t 'PY_PARSE_M1' '%s'" % str('>>'+str(in_args)+'<<'))
         tmp_p = argcomplete.argparse.ArgumentParser(add_help=False)
         tmp_p.add_argument('function_class', choices=avail_classes.values()).completer = parser_completer
         tmp_p = add_arg_help(tmp_p)
         argcomplete.autocomplete(tmp_p)
     p = argparse.ArgumentParser(description=mod.__doc__, formatter_class=argparse.RawTextHelpFormatter, add_help=False)
     p = add_arg_help(p)
+    # If no classes, send to parse as 'module'
     if len(avail_classes.values()) == 0:
         no_class, single_class = True, False
         return parse_module_or_class(mod, p, in_args, 'module')
+    # If Module only has one Class, this Class is called by default
     if len(avail_classes.values()) == 1:
         no_class = False
         single_class = avail_classes.values()[0]
         in_args.insert(0, single_class)
     else:
         no_class, single_class = False, False
+    # Parsing Ends Here
     if not single_class and (not in_args or in_args[0] == '--help'):
+        # if os.environ.has_key('COMP_LINE'):
+        #     os.system(                         "logger -t 'PY_PARSE_M2' '%s'" % str('>>'+os.environ['COMP_LINE']+'<<'))
+        # else:
+        #     os.system(                         "logger -t 'PY_PARSE_M2' '%s'" % str('>>'+str(in_args)+'<<'))
         tmp_usage = '\n\t' + p.format_usage().split()[1]
         sp = p.add_subparsers()
         for k, v in sorted(avail_classes.items()):
@@ -457,13 +549,23 @@ def parse_module(mod_path, regex, excludes):
         p.usage = tmp_usage + ' %s [--help]' % sp.metavar
         argcomplete.autocomplete(p)
         return p.parse_args()
+    # Unrecognized Parse instruction
     if not avail_classes.values().count(in_args[0]):
+        # if os.environ.has_key('COMP_LINE'):
+        #     os.system(                         "logger -t 'PY_PARSE_M3' '%s'" % str('>>'+os.environ['COMP_LINE']+'<<'))
+        # else:
+        #     os.system(                         "logger -t 'PY_PARSE_M3' '%s'" % str('>>'+str(in_args)+'<<'))
         print 'Unrecognized <class>'
         print in_args
         p.print_help()
         import sys
         sys.exit(1)
+    # Execute Parse instruction
     else:
+        # if os.environ.has_key('COMP_LINE'):
+        #     os.system(                         "logger -t 'PY_PARSE_M4' '%s'" % str('>>'+os.environ['COMP_LINE']+'<<'))
+        # else:
+        #     os.system(                         "logger -t 'PY_PARSE_M4' '%s'" % str('>>'+str(in_args)+'<<'))
         p_help = argparse.SUPPRESS if [ it for it in in_args if not it == '--help' ] else 'Functions to operate'
         p.add_argument('_class', help=p_help, choices=sorted(avail_classes.values()))
         p.usage = p.format_usage().replace('usage: ', '', 1).replace('[--help] {_class}', in_args[0] + ' [--help]')
